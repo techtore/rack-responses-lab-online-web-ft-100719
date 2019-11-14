ff4bf6720bea6ee7aa0895caf2dc2776512d0c04
Victoria Hale <vic.hale74@gmail.com>
2019-11-14 19:57:06 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -6,11 +6,10 @@ class Application
     
     if time < 12
       resp.write "Good Morning!"
-      
     else 
       resp.write "Good Afternoon!"
     end
-    resp.finish
+      resp.finish
   end
   
 end
